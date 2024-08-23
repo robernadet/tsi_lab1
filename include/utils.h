18aeef3bcb29fb3cf255a5a96cec0d00ce8535b1
robernadet <rociobernadet@gmail.com>
2024-08-23 20:55:53 -0300

    home

--- include/utils.h
+++ include/utils.h
@@ -1,7 +1,6 @@
 #ifndef UTILS
 #define UTILS
 
-#define GLOBAL_SEED_FILE "/etc/pam_seeds.txt"
 #define SEED_SIZE 20 // Length of HMAC-SHA1 output is 20 bytes (160 bits)
 #define PERIOD 30
 #define DIGITS 6
