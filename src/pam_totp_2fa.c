77b59da49ee18f58f905ebc0b0e63da14538b479
robernadet <rociobernadet@gmail.com>
2024-08-28 00:40:27 -0300

    add pass

--- src/pam_totp_2fa.c
+++ src/pam_totp_2fa.c
@@ -146,69 +146,69 @@ PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const c
   return PAM_SUCCESS;
 }
 
-// PAM_EXTERN int pam_sm_chauthtok(pam_handle_t *pamh, int flags, int argc, const char **argv)
-// {
-//   const char *user;
-//   int retval = pam_get_user(pamh, &user, "Username: ");
-//   if (retval != PAM_SUCCESS || user == NULL)
-//   {
-//     pam_syslog(pamh, LOG_ERR, "Error getting username");
-//     return PAM_USER_UNKNOWN;
-//   }
+PAM_EXTERN int pam_sm_chauthtok(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+  const char *user;
+  int retval = pam_get_user(pamh, &user, "Username: ");
+  if (retval != PAM_SUCCESS || user == NULL)
+  {
+    pam_syslog(pamh, LOG_ERR, "Error getting username");
+    return PAM_USER_UNKNOWN;
+  }
 
-//   const char *old_password;
-//   retval = pam_get_authtok(pamh, PAM_OLDAUTHTOK, &old_password, "Old Password: ");
-//   if (retval != PAM_SUCCESS || old_password == NULL)
-//   {
-//     pam_syslog(pamh, LOG_ERR, "Error getting old password");
-//     return PAM_AUTHTOK_ERR;
-//   }
+  const char *old_password;
+  retval = pam_get_authtok(pamh, PAM_OLDAUTHTOK, &old_password, "Old Password: ");
+  if (retval != PAM_SUCCESS || old_password == NULL)
+  {
+    pam_syslog(pamh, LOG_ERR, "Error getting old password");
+    return PAM_AUTHTOK_ERR;
+  }
 
-//   const char *new_password;
-//   retval = pam_get_authtok(pamh, PAM_AUTHTOK, &new_password, "New Password: ");
-//   if (retval != PAM_SUCCESS || new_password == NULL)
-//   {
-//     pam_syslog(pamh, LOG_ERR, "Error getting new password");
-//     return PAM_AUTHTOK_ERR;
-//   }
+  const char *new_password;
+  retval = pam_get_authtok(pamh, PAM_AUTHTOK, &new_password, "New Password: ");
+  if (retval != PAM_SUCCESS || new_password == NULL)
+  {
+    pam_syslog(pamh, LOG_ERR, "Error getting new password");
+    return PAM_AUTHTOK_ERR;
+  }
 
-//   // Verificar que la semilla anterior pueda ser descifrada con la contraseña antigua
-//   size_t encrypted_len;
-//   char *encrypted_seed = getEncryptedSeedForUser(user, &encrypted_len);
-//   if (encrypted_seed == NULL)
-//   {
-//     pam_syslog(pamh, LOG_ERR, "Seed not found for user: %s", user);
-//     return PAM_AUTHTOK_ERR;
-//   }
+  // Verificar que la semilla anterior pueda ser descifrada con la contraseña antigua
+  size_t encrypted_len;
+  char *encrypted_seed = getEncryptedSeedForUser(user, &encrypted_len);
+  if (encrypted_seed == NULL)
+  {
+    pam_syslog(pamh, LOG_ERR, "Seed not found for user: %s", user);
+    return PAM_AUTHTOK_ERR;
+  }
 
-//   char *decrypted_seed = decrypt_seed(encrypted_seed, encrypted_len, old_password);
-//   if (decrypted_seed == NULL)
-//   {
-//     pam_syslog(pamh, LOG_ERR, "Error decrypting seed with old password for user: %s", user);
-//     free(encrypted_seed);
-//     return PAM_AUTHTOK_ERR;
-//   }
+  char *decrypted_seed = decrypt_seed(encrypted_seed, encrypted_len, old_password);
+  if (decrypted_seed == NULL)
+  {
+    pam_syslog(pamh, LOG_ERR, "Error decrypting seed with old password for user: %s", user);
+    free(encrypted_seed);
+    return PAM_AUTHTOK_ERR;
+  }
 
 
-//   // Encriptar la nueva semilla con la nueva contraseña
-//   size_t new_encrypted_len;
-//   char *new_encrypted_seed = encrypt_seed(decrypted_seed, new_password, &new_encrypted_len);
-//   if (new_encrypted_seed == NULL)
-//   {
-//     pam_syslog(pamh, LOG_ERR, "Error encrypting new seed for user: %s", user);
-//     free(encrypted_seed);
-//     free(decrypted_seed);
-//     return PAM_AUTHTOK_ERR;
-//   }
+  // Encriptar la nueva semilla con la nueva contraseña
+  size_t new_encrypted_len;
+  char *new_encrypted_seed = encrypt_seed(decrypted_seed, new_password, &new_encrypted_len);
+  if (new_encrypted_seed == NULL)
+  {
+    pam_syslog(pamh, LOG_ERR, "Error encrypting new seed for user: %s", user);
+    free(encrypted_seed);
+    free(decrypted_seed);
+    return PAM_AUTHTOK_ERR;
+  }
 
-//   // Guardar la nueva semilla encriptada
-//   saveEncryptedSeedToFile(new_encrypted_seed, new_encrypted_len, user);
-//   pam_syslog(pamh, LOG_INFO, "Updated seed for user: %s after password change", user);
+  // Guardar la nueva semilla encriptada
+  saveEncryptedSeedToFile(new_encrypted_seed, new_encrypted_len, user);
+  pam_syslog(pamh, LOG_INFO, "Updated seed for user: %s after password change", user);
 
-//   // Liberar memoria
-//   free(encrypted_seed);
-//   free(decrypted_seed);
-//   free(new_encrypted_seed);
+  // Liberar memoria
+  free(encrypted_seed);
+  free(decrypted_seed);
+  free(new_encrypted_seed);
 
-//   return PAM_SUCCESS;
-// }
+  return PAM_SUCCESS;
+}
